9d7948a6c468cfd63fc784cf1614f653146a483e
jgotti1 <96301204+jgotti1@users.noreply.github.com>
2022-08-09 22:04:54 -0400

    added search

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,6 +7,7 @@ import { useAuthContext } from "../hooks/useAuthContext";
 
 function Home() {
   const [workouts, setWorkouts] = useState(null);
+  const [search, setSearch] = useState("");
   const { user } = useAuthContext();
 
   useEffect(() => {
@@ -25,11 +26,53 @@ function Home() {
     }
   }, [user]);
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(search);
+    const resultsArray = workouts.filter((workout) => workout.title.includes(search) || workout.details.includes(search));
+    console.log(resultsArray);
+    setWorkouts(resultsArray);
+
+    return workouts;
+  };
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    const fetchWorkouts = async () => {
+      const response = await fetch(workoutFetchPath, {
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
+      const json = await response.json();
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    if (user) {
+      fetchWorkouts();
+    }
+  };
+
   return (
     <>
+      <form onSubmit={handleSearch}>
+        <label className="search">
+          Search Workouts...
+          <input className="searchWorkouts" type="text" placeholder="? ? ?" onChange={(e) => setSearch(e.target.value)} />
+          <span className="buttons_search">
+            <button>Search</button>
+            <button onClick={handleClear}>Clear Search</button>
+          </span>
+        </label>
+      </form>
+
       <div className="home">
         <WorkoutForm />
-        <div className="workouts">{workouts && workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout} />)}</div>
+
+        <div className="workouts">
+          <h1 className="header_text">WORKOUTS</h1>
+          {workouts && workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout} />)}
+        </div>
       </div>
     </>
   );
